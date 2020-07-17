b82150133b56f4766c1d984831423806b8c0ad75
Bruno <mguimaraesbruno@gmail.com>
2020-07-16 21:37:56 -0300

    add tests with states

--- src/components/AbsenceTable/tests/index.test.tsx
+++ src/components/AbsenceTable/tests/index.test.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import AbsenceTable from '..';
+import { setHookState } from '../../../utils/test';
+
+const reactMock = require('react')
 
 describe('AbsenceTable', () => {
-  it('renders the component', () => {
-    const component = shallow(<AbsenceTable />);
+  let wrapper;
+  
+  beforeAll(() => {  
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }))
+    });
+  });
 
-    // expect(component.find('Table').props().);
-    expect(component).toMatchSnapshot();
+  it('renders the component', () => {
+    wrapper = shallow(<AbsenceTable />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders the table with absences', () => {
+    wrapper = mount(<AbsenceTable />);
+
+    reactMock.useState = setHookState({
+      absencesWithEmployee: ["a lot of stuff"],
+  })
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find('Table')).toMatchSnapshot();
+  expect(wrapper.find('Table').exists()).toEqual(true)
   });
 });
 
--- src/components/CalendarComponent/tests/index.test.tsx
+++ src/components/CalendarComponent/tests/index.test.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import CalendarComponent from '..';
+import { setHookState } from '../../../utils/test';
+
+const reactMock = require('react')
 
 describe('CalendarComponent', () => {
+  let wrapper;
+
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }))
+    });
+  });
+
   it('renders the component', () => {
-    const component = shallow(<CalendarComponent />);
-    expect(component).toMatchSnapshot();
+    wrapper = shallow(<CalendarComponent />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders the table with events', () => {
+    wrapper = mount(<CalendarComponent />);
+
+    reactMock.useState = setHookState({
+      events: ["a lot of events"],
+    })
+
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.find('Calendar')).toMatchSnapshot();
+    expect(wrapper.find('Calendar').exists()).toEqual(true)
   });
 });
-
